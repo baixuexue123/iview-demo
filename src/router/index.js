733a21de92366108923c95c843e47bb921d57a22
jdbxu <baixue12@jd.com>
2018-08-07 19:05:44 +0800

    loading bar

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import iView from 'iview';
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login.vue')
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start();
+})
+
+router.afterEach((to) => {
+    iView.LoadingBar.finish();
+    window.scrollTo(0, 0);
+});
+
+export default router;
