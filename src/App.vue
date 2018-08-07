6a4fe305c1f492defdc58e6b0cbc211bfae601ad
jdbxu <baixue12@jd.com>
2018-08-07 17:53:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+    export default {
+        data () {
+        },
+        mounted () {
+        },
+        beforeDestroy () {
+        },
+        methods: {
+        }
+    };
+</script>
+
+<style>
+html,body{
+    width: 100%;
+    height: 100%;
+    background: #f0f0f0;
+    overflow: hidden;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import iView from 'iview';
+import App from './App.vue';
+import router from './router/index.js';
+import 'iview/dist/styles/iview.css';
+
+Vue.use(iView);
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App),
+    data: {
+        currentPageName: ''
+    },
+    mounted () {
+        this.currentPageName = this.$route.name;
+    },
+    created () {
+    }
+});
